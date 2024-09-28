b7191ea6e83549a5b4152fa0a684162a8e6cc080
Evelyn-Epiousion <evelyn.alcantara@epiousion.com.br>
2024-09-28 16:48:54 -0300

    first commit

--- src/modules/admin/Register.tsx
+++ src/modules/admin/Register.tsx
@@ -1,7 +1,106 @@
+import { Button, Card, Col, Divider, Form, Input, Row } from "antd";
+import TextArea from "antd/es/input/TextArea";
+
 export default function Register() {
   return (
-    <div>
-      <p>Register</p>
-    </div>
+    <Row gutter={[16, 16]}>
+      <Col span={12} style={{ height: "100%" }}>
+        <Card
+          title="Cadastrar Vaga"
+          style={{ padding: "24px", height: "100%" }}
+          size="small"
+        >
+          <Form layout="vertical" size="large">
+            <Row gutter={[16, 2]}>
+              <Col span={24}>
+                <Form.Item label="Nome da Vaga" name="name">
+                  <Input />
+                </Form.Item>
+              </Col>
+
+              <Col span={24}>
+                <Form.Item label="Nome da Empresa" name="nomeEmpresa">
+                  <Input />
+                </Form.Item>
+              </Col>
+
+              <Col span={12}>
+                <Form.Item label="Cargo" name="cargo">
+                  <Input />
+                </Form.Item>
+              </Col>
+
+              <Col span={12}>
+                <Form.Item label="Formação exigida" name="formacao">
+                  <Input />
+                </Form.Item>
+              </Col>
+
+              <Col span={24}>
+                <Form.Item label="Descrição da Vaga" name="descricao">
+                  <Input />
+                </Form.Item>
+              </Col>
+
+              <Col span={24}>
+                <Form.Item label="Formação acadêmica" name="academic">
+                  <TextArea rows={4} />
+                </Form.Item>
+              </Col>
+
+              <Col span={24}>
+                <Form.Item label="Corpo do curriculo Lattes" name="resume">
+                  <TextArea rows={4} />
+                </Form.Item>
+              </Col>
+            </Row>
+
+            <Divider>Endereço</Divider>
+
+            <Row gutter={[16, 8]}>
+              <Col span={8}>
+                <Form.Item label="Digite seu CEP" name="cep">
+                  <Input />
+                </Form.Item>
+              </Col>
+
+              <Col span={18}>
+                <Form.Item label="Endereço" name="address">
+                  <Input />
+                </Form.Item>
+              </Col>
+
+              <Col span={6}>
+                <Form.Item label="Número" name="number">
+                  <Input />
+                </Form.Item>
+              </Col>
+
+              <Col span={8}>
+                <Form.Item label="Complemento" name="complement">
+                  <Input />
+                </Form.Item>
+              </Col>
+
+              <Col span={8}>
+                <Form.Item label="Cidade" name="city">
+                  <Input />
+                </Form.Item>
+              </Col>
+
+              <Col span={8}>
+                <Form.Item label="Estado" name="state">
+                  <Input />
+                </Form.Item>
+              </Col>
+
+              <Col span={24}>
+                <Button onClick={() => ""}>Cadastrar-se</Button>
+              </Col>
+            </Row>
+          </Form>
+        </Card>
+      </Col>
+    </Row>
   );
 }
